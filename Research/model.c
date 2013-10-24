421f567f5f3b31a3336e3a98b295b2fd37d66213
Peter Wei <PeterJWei1993@gmail.com>
2013-10-24 18:35:22 -0400

    Added Delays

--- Research/model.c
+++ Research/model.c
@@ -6,8 +6,8 @@
     int strength;
     int delay;
 };*/
-int maxDelay(int* poss, int n, char** vars);
-void printRules(char **rules, int num, char* str);
+int maxDelay(int* arrB, int arrSize);
+void printRules(char ***rules, int num, char* str, int systemMax);
 
 int n = 5;
 
@@ -23,17 +23,17 @@ int main() {
         int* arrB = t[i] -> arrB;
         int n = t[i] -> n;
         int arrSize = t[i] -> arrSize;
-        MD = maxDelay(poss, n, vars);
+        MD = maxDelay(arrB, arrSize);
         molNum = n;
         printf("////////////////////////////////////////////////////\n");
         printf("test number %d, molecule %s, %d inputs\n", i, vars[n - 1], n - 1);
         printf("////////////////////////////////////////////////////\n");
-        model(arrA, arrB, arrSize, MD, vars, poss, molNum);
+        model(arrA, arrB, arrSize, MD, vars, poss, molNum, 3);
     }
     //molecule strength array, delay array, maximum delay given, molecule variable names, possible strengths, number of vars
 }
 
-int model(int *arrA, int *arrB, int arrSize, int maxDelay, char **vars, int *poss, int molNums) {
+int model(int *arrA, int *arrB, int arrSize, int maxDelay, char **vars, int *poss, int molNums, int systemMax) {
     int i, j, k;
     int len;
     int numDelays = maxDelay;
@@ -42,55 +42,81 @@ int model(int *arrA, int *arrB, int arrSize, int maxDelay, char **vars, int *pos
     char* str;
     char format[200];
     int track;
+//    int delay = maxDelay(arrB, arrSize);
     int num = poss[limit];
-    char** rules = malloc(sizeof(char*) * num); //strings of rules
+    //set delay trackers
+    
+    char*** rules = malloc(sizeof(char**) * num); //strings of rules
     for (i = 0; i < num; i++) {
-        rules[i] = malloc(sizeof(char) * 10000);
-        strcpy(rules[i], "");
+        rules[i] = malloc(sizeof(char *) * maxDelay + 1);
+    }
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < maxDelay + 1; j++) {
+            rules[i][j] = malloc(sizeof(char) * 10000);
+            strcpy(rules[i][j], "");
+        }
     }
 
     for (j = 0; j < arrSize; j++) {
         track = j;
-        strcat(rules[arrA[j]], "(");
+        int del = arrB[j];
+        strcat(rules[arrA[j]][0], "(");
         for (i = limit - 1; i >= 1; i--) {
-            mod = track % poss[i];
-            str = vars[i];
-            sprintf(format, "%s_%d  and  ", str, mod);
-            strcat(rules[arrA[j]], format);
-            track = track/poss[i];
+            for (k = 0; k < del + 1; k++) {
+                mod = track % poss[i];
+                str = vars[i];
+                if (k != 0) {
+                    sprintf(format, "%s_%d_del%d  and  ", str, mod, k);
+                }
+                else {
+                    sprintf(format, "%s_%d  and  ", str, mod, k);
+                }
+                strcat(rules[arrA[j]][0], format);
+                track = track/poss[i];
+            }
         }
         mod = track % poss[0];
         str = vars[0];
         sprintf(format, "%s_%d)", str, mod);
-        strcat(rules[arrA[j]], format);
+        strcat(rules[arrA[j]][0], format);
         track = track/poss[0];
 
-        strcat(rules[arrA[j]], " or\n");
+        strcat(rules[arrA[j]][0], " or\n");
     }
     for (k = 0; k < num; k++) {
-        len = strlen(rules[k]);
-        strcpy((rules[k] + len - 4), "\0");
+        len = strlen(rules[k][0]);
+        strcpy((rules[k][0] + len - 4), "\0");
     }
-    printRules(rules, num, vars[limit]);
+    printRules(rules, num, vars[limit], systemMax);
     free(rules);
 }
 
-void printRules(char **rules, int num, char* str) {
-    int i;
+void printRules(char ***rules, int num, char* str, int systemMax) {
+    int i, j;
     for (i = 0; i < num; i++) {
         printf("%s", str);
         printf("_%d:\n", i);
-        printf("%s\n", rules[i]);
-        free(rules[i]);
+        printf("%s\n", rules[i][0]);
+    }
+    for (i = 0; i < num; i++) {
+        for (j = 1; j < systemMax + 1; j++) {
+            if (j != 1) {
+                printf("%s_%d_del%d = %s_%d_del%d\n", str, i, j, str, i, j - 1);
+            }
+            else {
+                printf("%s_%d_del%d = %s_%d\n", str, i, j, str, i);
+            }
+        }
+//        free(rules[i]);
     }
 }
 
-int maxDelay(int* poss, int n, char** vars) {
+int maxDelay(int* arrB, int arrSize) {
     int i;
     int max = 0;
-    for (i = 0; i < n; i++) {
-        if (poss[i] > max && strcmp(vars[i], "ERK_ACT")) {
-            max = poss[i];
+    for (i = 0; i < arrSize; i++) {
+        if (arrB[i] > max) {
+            max = arrB[i];
         }
     }
     return max;
